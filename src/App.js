c6006bfd6709857305539b92667ee86a074fc668
Alex <alexnaunt@hotmail.com>
2019-09-28 09:18:13 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Player } from 'video-react';
+import {Navbar, NavDropdown, Nav, Form, Button, FormControl, Container} from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+  <Navbar bg="light" className = "Navbar">
+  <Navbar.Brand href="#home">Codo$</Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">Video</Nav.Link>
+      <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1"></NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+  </Navbar>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Codo$ 
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.facebook.com/neurofinanzasac/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Saber mas
         </a>
       </header>
+      <Container className ="Container">
+      <Player
+      playsInline
+      poster="/assets/poster.png"
+      src="C:\Users\alexn\NeuroFinanzas\Landing\FirstComponent\my_last_app\src\asses\dinero.mp4"
+    />
+      </Container>
     </div>
   );
 }
